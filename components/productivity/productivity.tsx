618a10763ec1e525654c84a6327e8baa737c9643
c-correa <19correa99@gmail.com>
2024-10-31 23:13:06 -0500

    Update productivity.tsx
    fix

--- components/productivity/productivity.tsx
+++ components/productivity/productivity.tsx
@@ -14,13 +14,13 @@ import Zap from "@/public/svg/Zap";
 const Productivity: React.FC = () => {
   return (
     <>
-      <section className="flex flex-col gap-10 items-center lg:!flex-row lg:container lg:gap-0 lg:mx-auto lg:p-28 md:py-[72px] py-14 relative">
-        <div className="container box-c lg:pl-0 lg:pr-12 lg:w-1/2 mx-auto px-6 relative self-stretch shrink top-0 xl:pr-20">
+      <section className="container flex flex-col gap-10 items-center lg:!flex-row lg:gap-0 lg:mx-auto md:py-[72px] py-14 relative">
+        <div className="box-c lg:pl-0 lg:pr-12 lg:w-1/2 mx-auto px-6 relative self-stretch shrink top-0 xl:pr-20">
           <div className="sticky bottom-0 top-[calc(var(--header-height)+40px)] flex flex-col gap-10">
             <div className="flex flex-col gap-3 self-start items-start">
               <div className="flex justify-center items-center bg-[#1FDD8A]/10 rounded-[20px] ">
                 <p className="text-dark-text-tertiary opacity-50 p-2">
-                  Productivity
+                Innovación
                 </p>
               </div>
               <div className="flex max-w-[800px] flex-col justify-center gap-1 items-start self-start">
@@ -28,12 +28,11 @@ const Productivity: React.FC = () => {
                   className="text-pretty text-3xl font-medium md:text-4xl text-left"
                   title="Supercharge Team Productivity"
                 >
-                  Supercharge Team Productivity
+                  Potenciamos el futuro con soluciones digitales de última generación
                 </h4>
               </div>
-              <p className="max-w-screen-md text-pretty text-lg font-light text-text-tertiary dark:text-dark-text-tertiary md:text-xl text-left">
-                Keep your team focused and productive as they collaborate on
-                building and shipping products swiftly.
+              <p className="max-w-screen-md !text-base text-pretty font-light text-text-tertiary dark:text-dark-text-tertiary md:text-xl text-left">
+              Aprovechamos el poder de la inteligencia artificial para crear soluciones digitales de última generación, impulsando eficiencia y crecimiento en cada proyecto. Con tecnología avanzada y un enfoque personalizado, te ayudamos a enfrentar los desafíos del futuro, hoy.
               </p>
             </div>
             <div className="flex items-center gap-3 md:order-3">
@@ -57,14 +56,14 @@ const Productivity: React.FC = () => {
             <article className="flex w-[300px] shrink-0 gap-4 rounded-lg  bg-surface-secondary p-4 dark:border-dark-border dark:bg-dark-surface-secondary lg:w-full lg:flex-row lg:p-5">
               <Card className="flex">
                 <CardHeader className="flex-row align-center gap-3">
-                  <Mail width={54} height={54} fill="white" />
+                  <Mail width={40} height={40} fill="white" />
                 </CardHeader>
                 <CardContent className="p-3">
-                  <CardTitle className="text-3xl mb-3">
-                    Real-time Messaging
+                  <CardTitle className=" mb-3">
+                  Desarrollo de Software a Medida
                   </CardTitle>
-                  <p>
-                    Instantly communicate with your team, ensuring swift decision-making and seamless collaboration on project tasks and updates.
+                  <p className="!text-base opacity-50">
+                  Creación de aplicaciones web y móviles personalizadas según las necesidades específicas de cada cliente.
                   </p>
                 </CardContent>
               </Card>
@@ -72,14 +71,14 @@ const Productivity: React.FC = () => {
             <article className="flex w-[300px] shrink-0 gap-4 rounded-lg bg-surface-secondary p-4 dark:border-dark-border dark:bg-dark-surface-secondary lg:w-full lg:flex-row lg:p-5">
               <Card className="flex">
                 <CardHeader className="flex-row align-center gap-3">
-                  <Zap width={54} height={54} fill="white" />
+                  <Zap width={40} height={40} fill="white" />
                 </CardHeader>
                 <CardContent className="p-3">
-                  <CardTitle className="text-3xl mb-3">
-                    Task Management
+                  <CardTitle className=" mb-3">
+                  Inteligencia Artificial y Machine Learning
                   </CardTitle>
-                  <p>
-                    Organize and prioritize tasks effectively, assigning responsibilities and tracking progress to keep projects on schedule and within scope.
+                  <p className="!text-base opacity-50">
+                  Soluciones de IA para análisis de datos, automatización de procesos y mejora de la experiencia de usuario a través de recomendaciones y predicciones.
                   </p>
                 </CardContent>
               </Card>
@@ -87,14 +86,14 @@ const Productivity: React.FC = () => {
             <article className="flex w-[300px] shrink-0 gap-4 rounded-lg bg-surface-secondary p-4 dark:border-dark-border dark:bg-dark-surface-secondary lg:w-full lg:flex-row lg:p-5">
               <Card className="flex">
                 <CardHeader className="flex-row align-center gap-3">
-                  <BarChar width={54} height={54} fill="white" />
+                  <BarChar width={40} height={40} fill="white" />
                 </CardHeader>
                 <CardContent className="p-3">
-                  <CardTitle className="text-3xl mb-3">
-                    File Sharing
+                  <CardTitle className=" mb-3">
+                  Desarrollo de APIs e Integración de Servicios Cloud
                   </CardTitle>
-                  <p>
-                    Share documents, images, and other files effortlessly within your team, enabling easy access to project resources and materials.
+                  <p className="!text-base opacity-50">
+                  Conexión de sistemas y datos mediante APIs, y migración a la nube para optimizar recursos (AWS, Azure, Google Cloud).
                   </p>
                 </CardContent>
               </Card>
@@ -102,14 +101,14 @@ const Productivity: React.FC = () => {
             <article className="flex w-[300px] shrink-0 gap-4 rounded-lg bg-surface-secondary p-4 dark:border-dark-border dark:bg-dark-surface-secondary lg:w-full lg:flex-row lg:p-5">
               <Card className="flex">
                 <CardHeader className="flex-row align-center gap-3">
-                  <Smile width={54} height={54} fill="white" />
+                  <Smile width={40} height={40} fill="white" />
                 </CardHeader>
                 <CardContent className="p-3">
-                  <CardTitle className="text-3xl mb-3">
-                    Real-time Insights
+                  <CardTitle className=" mb-3">
+                  Automatización de Procesos (RPA)
                   </CardTitle>
-                  <p>
-                    Gain actionable insights instantly with real-time data analysis and visualization.
+                  <p className="!text-base opacity-50">
+                  Implementación de robots de software para automatizar tareas repetitivas, mejorando la eficiencia operativa de las empresas.
                   </p>
                 </CardContent>
               </Card>
@@ -117,14 +116,14 @@ const Productivity: React.FC = () => {
             <article className="flex w-[300px] shrink-0 gap-4 rounded-lg bg-surface-secondary p-4 dark:border-dark-border dark:bg-dark-surface-secondary lg:w-full lg:flex-row lg:p-5">
               <Card className="flex">
                 <CardHeader className="flex-row align-center gap-3">
-                  <Command width={54} height={54} fill="white" />
+                  <Command width={40} height={40} fill="white" />
                 </CardHeader>
                 <CardContent className="p-3">
-                  <CardTitle className="text-3xl mb-3">
-                    Smart Notifications
+                  <CardTitle className=" mb-3">
+                  Transformación Digital y Modernización de TI
                   </CardTitle>
-                  <p>
-                    Stay informed about project updates and important discussions without being overwhelmed, thanks to customizable notification settings.
+                  <p className="!text-base opacity-50">
+                  Asesoría para adaptar los sistemas de las empresas a entornos digitales y mejorar su infraestructura tecnológica.
                   </p>
                 </CardContent>
               </Card>
@@ -132,16 +131,29 @@ const Productivity: React.FC = () => {
             <article className="flex w-[300px] shrink-0 gap-4 rounded-lg bg-surface-secondary p-4 dark:border-dark-border dark:bg-dark-surface-secondary lg:w-full lg:flex-row lg:p-5">
               <Card className="flex">
                 <CardHeader className="flex-row align-center gap-3">
-                  <Message width={54} height={54} fill="white" />
+                  <Message width={40} height={40} fill="white" />
                 </CardHeader>
                 <CardContent className="p-3">
-                  <CardTitle className="text-3xl mb-3">
-                    Team Analytics
+                  <CardTitle className=" mb-3">
+                  Consultoría en Experiencia del Usuario (UX/UI)
                   </CardTitle>
-                  <p>
-                    Gain insights into team performance and communication trends
-                    with built-in analytics, empowering you to optimize
-                    workflows and enhance productivity.
+                  <p className="!text-base opacity-50">
+                  Diseño de interfaces y flujos de usuario intuitivos para aplicaciones que generen una experiencia positiva.
+                  </p>
+                </CardContent>
+              </Card>
+            </article>
+            <article className="flex w-[300px] shrink-0 gap-4 rounded-lg bg-surface-secondary p-4 dark:border-dark-border dark:bg-dark-surface-secondary lg:w-full lg:flex-row lg:p-5">
+              <Card className="flex">
+                <CardHeader className="flex-row align-center gap-3">
+                  <Message width={40} height={40} fill="white" />
+                </CardHeader>
+                <CardContent className="p-3">
+                  <CardTitle className=" mb-3">
+                  Desarrollo y Optimización de E-commerce
+                  </CardTitle>
+                  <p className="!text-base opacity-50">
+                  Creación y optimización de plataformas de comercio electrónico, incluyendo personalización de la experiencia de usuario y funciones de venta avanzadas.
                   </p>
                 </CardContent>
               </Card>
